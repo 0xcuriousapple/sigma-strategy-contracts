929449cb3da5bec2653c4a4e0ae4775764533671
scdev157 <scdev@anon.farm>
2021-09-27 04:21:03 +0530

    Fix : Testing nonce issue

--- test/03_vault.deposit.ts
+++ test/03_vault.deposit.ts
@@ -5,17 +5,16 @@ import { ethers, deployments } from 'hardhat';
 import { SigmaVaultDetails } from '../constants.json';
 import stealFunds from './utils/stealFunds';
 
-describe('SigmaVault', async function () {
-  await deployments.fixture(['SigmaVault', 'SigmaStrategy', 'PostDeploymentSetup']);
-  const SigmaVault = await ethers.getContract('SigmaVault');
-  const token0Address = await SigmaVault.token0();
-  const token1Address = await SigmaVault.token1();
-
-  const signers = await ethers.getSigners();
-  const token0 = new ethers.Contract(token0Address, erc20ABI, signers[0]);
-  const token1 = new ethers.Contract(token1Address, erc20ABI, signers[0]);
-
+describe('SigmaVault', function () {
   it('First Deposit', async function () {
+    await deployments.fixture(['SigmaVault', 'SigmaStrategy', 'PostDeploymentSetup']);
+    const SigmaVault = await ethers.getContract('SigmaVault');
+    const token0Address = await SigmaVault.token0();
+    const token1Address = await SigmaVault.token1();
+
+    const signers = await ethers.getSigners();
+    const token0 = new ethers.Contract(token0Address, erc20ABI, signers[0]);
+    const token1 = new ethers.Contract(token1Address, erc20ABI, signers[0]);
     await stealFunds(0, signers[0].address, '10');
     await stealFunds(1, signers[0].address, '100000');
     await token0.approve(SigmaVault.address, tokenAmount('10', 18));
@@ -31,9 +30,4 @@ describe('SigmaVault', async function () {
     const sharesMinted = await SigmaVault.balanceOf(signers[0].address);
     expect(sharesMinted).to.equal(tokenAmount('10', 18));
   });
-
-  // Second Deposit
-  it('Second Deposit', async function () {});
-
-  it('Deposit After Rebalance', async function () {});
 });
